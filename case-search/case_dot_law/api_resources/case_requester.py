43c57b99a8d79caa6533d20b7878ddbe3f600bae
Samier Saeed <samiersaeed@gmail.com>
2020-06-14 11:37:55 -0700

    another abstraction layer

---
+++ case-search/case_dot_law/api_resources/case_requester.py
@@ -0,0 +1,8 @@
+from case_dot_law.api_resources.api_requester import ApiRequester
+
+class CaseRequester(ApiRequester):
+    def __init__(self):
+        super().__init__("cases")
+    
+    def get(self):
+        print(self.url)
