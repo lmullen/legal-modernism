437094c95f235fe8545b7060df14f4385a21d44d
Samier Saeed <samiersaeed@gmail.com>
2020-06-14 13:21:22 -0700

    added comment

--- case-search/tests/test_api_params.py
+++ case-search/tests/test_api_params.py
@@ -3,6 +3,7 @@ import pytest
 from case_dot_law.api_resources.params.api_params import ApiParams
 
 def test_append_question_mark():
+    # Kind of unncessary since, after stubbing out the test, I realized I should find a library to do all the URL stuff, and found furl.
     test_url = "google.com"
     test_params = ApiParams()
 
