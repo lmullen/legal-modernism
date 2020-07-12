d9c4557a6898637127eba6cd3be67293412d7048
Samier Saeed <samiersaeed@gmail.com>
2020-07-12 16:08:03 -0700

    array of citations

--- citation-detection/src/extraction.ts
+++ citation-detection/src/extraction.ts
@@ -2,15 +2,24 @@ import * as _regex from "xregexp";
 
 import * as testTexts from "./test_text";
 
-let catchallRegex : RegExp = /[\d]{1,3}\s[a-zA-Z0-9\.]+\s[\d]{1,4}/g;
+let catchallRegex: RegExp = /[\d]{1,3}\s[a-zA-Z0-9\.]+\s[\d]{1,4}/g;
 
-// testTexts.manyCitations0.matchAll(catchallRegex).forEach((x) => {
-//     console.log(x);
-// });
-let res;
-let match;
-let result;
+export async function extractMatches(text: string, regex: RegExp): Promise<string[]> {
+    let res = [];
+    let match;
 
-while ((result = catchallRegex.exec(testTexts.manyCitations0)) !== null) {
-    console.log(result[0]);
+    while ((match = regex.exec(text)) !== null) {
+        // console.log(match[0]);
+        res.push(match[0]);
+    }
+
+    return res;
 }
+
+async function main() {
+    let _res = await extractMatches(testTexts.manyCitations0, catchallRegex);
+    console.log(_res);
+}
+
+main();
+
