929683c406e9ce11c629a028d7de30fbccd75e5c
Samier Saeed <samiersaeed@gmail.com>
2020-07-12 14:55:09 -0700

    amazement

--- citation-detection/src/extraction.ts
+++ citation-detection/src/extraction.ts
@@ -1,11 +1,16 @@
+import * as _regex from "xregexp";
+
 import * as testTexts from "./test_text";
 
-let catchallRegex : RegExp = /[\d]{1,3}\s[a-zA-Z0-9\.]+\s[\d]{1,4}/;
+let catchallRegex : RegExp = /[\d]{1,3}\s[a-zA-Z0-9\.]+\s[\d]{1,4}/g;
 
-// let res = catchallRegex.exec(testTexts.manyCitations0);
-let res = testTexts.manyCitations0.matchAll(catchallRegex);
-
-console.log(res);
-// for(var x in res){
+// testTexts.manyCitations0.matchAll(catchallRegex).forEach((x) => {
 //     console.log(x);
-// }
+// });
+let res;
+let match;
+let result;
+
+while ((result = catchallRegex.exec(testTexts.manyCitations0)) !== null) {
+    console.log(result[0]);
+}
