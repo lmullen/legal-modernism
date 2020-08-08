b88318092b8d9ec8abd1e64d52943c1f4fe6bd40
Samier Saeed <samiersaeed@gmail.com>
2020-08-08 14:56:12 -0700

    @TODO

--- citation-detection/src/reporters/reporters.ts
+++ citation-detection/src/reporters/reporters.ts
@@ -20,107 +20,143 @@ let reporterList = [
     {
         name: "Dallam",
         caseDotLawID: 929,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Aikens",
         caseDotLawID: 661,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Alabama Reports",
         caseDotLawID: 296,
-        regEx: /[\d]{1,3}\sAla\.\s[\d]{1,4}/g 
+        regEx: /[\d]{1,3}\sAla\.\s[\d]{1,4}/g
     },
     {
         name: "Alaska Reports",
         caseDotLawID: 596,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Alaska Federal Reports",
         caseDotLawID: 597,
-        regEx: null 
+        regEx: null
     },
     {
         name: "American Somoa Reports",
         caseDotLawID: 951,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Anthon's Nisi Prius Cases",
         caseDotLawID: 119,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Appellate Division Reports",
         caseDotLawID: 107,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Arizona Reports",
         caseDotLawID: 291,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Arkansas Reports",
         caseDotLawID: 368,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Armstrong's Contested Election Cases in New York",
         caseDotLawID: 135,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Baltimore City Reports",
         caseDotLawID: 735,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Barbour's Chancery Reports",
         caseDotLawID: 2,
-        regEx: /[\d]{1,3}\sBarb\.\sCh\.\s[\d]{1,4}/g 
+        regEx: /[\d]{1,3}\sBarb\.\sCh\.\s[\d]{1,4}/g
     },
     {
         name: "Barbour's Supreme Court Reports",
         caseDotLawID: 112,
-        regEx: /[\d]{1,3}\sBarb\.\s[\d]{1,4}/g 
+        regEx: /[\d]{1,3}\sBarb\.\s[\d]{1,4}/g
     },
     {
         name: "Blackford",
         caseDotLawID: 643,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Bosworth",
         caseDotLawID: 175,
-        regEx: null 
+        regEx: null
     },
     {
         name: "Bradford's Reports",
         caseDotLawID: 131,
-        regEx: null 
+        regEx: null
     },
-   {
+    {
         name: "Brayton",
         caseDotLawID: 663,
-        regEx: null 
+        regEx: null
     },
-   {
+    {
         name: "Burnett",
         caseDotLawID: 656,
-        regEx: null 
+        regEx: null
     },
-   {
+    {
         name: "Decisions of the Hon. John Cadwalader",
         caseDotLawID: 788,
-        regEx: null 
+        regEx: null
     },
-  {
+    {
         name: "Caines' Cases", //sic
         caseDotLawID: 124,
-        regEx: /[\d]{1,3}\sCai\.\s[\d]{1,4}/g 
+        alternateIDs: [123],
+        regEx: /[\d]{1,3}\sCai\.\s[\d]{1,4}/g
+    },
+    {
+        name: "California Appellate Reports",
+        caseDotLawID: 327,
+        regEx: null
+    },
+    {
+        name: "California Appellate Reports, Second Series",
+        caseDotLawID: 329,
+        regEx: null
+    },
+    {
+        name: "California Reports",
+        caseDotLawID: 414,
+        regEx: /[\d]{1,3}\sCal\.\s[\d]{1,4}/g
+    },
+    {
+        name: "California Unreported Cases",
+        caseDotLawID: 565,
+        regEx: /[\d]{1,3}\sCal\.\sUnrep\.\s(R\.\s)?[\d]{1,4}/g
+    },
+    {
+        name: "Cases decided in the Courts of common pleas, in the Fifth circuit of the state of Ohio [1816-1819] (Tappan)",
+        caseDotLawID: 773,
+        regEx: null
+    },
+    {
+        name: "Ohio Supreme Court special session",
+        caseDotLawID: 781,
+        regEx: null
+    },
+    {
+        name: "Ohio State", //@TODO Go through ToA and put unrepresented reporters in here.
+        caseDotLawID: 781,
+        regEx: null
     },
     {
         name: "Howard's Practice Reports",
