56eeec0031e673b1c1e4f45b6cc34f9a476ce0f3
Samier Saeed <samier_saeed@brown.edu>
2020-10-18 11:23:47 -0700

    function to get specific reporter

--- citation-detection/src/reporters/index.ts
+++ citation-detection/src/reporters/index.ts
@@ -3,5 +3,11 @@ import * as _ from "lodash";
 import reporterList from './reporters';
 
 export async function getReporterByID(id: number) {
-    return _.find(reporterList, (r) => {r.caseDotLawID == id});
+    // return _.find(reporterList, (r) => {r.caseDotLawID == id});
+    let p = (r) => {
+        return r.caseDotLawID == id;
+    }
+
+    let filtered = reporterList.filter(p);
+    return filtered[0];
 }
