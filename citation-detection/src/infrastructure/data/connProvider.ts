0efa7e44f0c0805407b29a75f9d07b1df191d4a7
Samier Saeed <samier_saeed@brown.edu>
2020-11-23 09:51:01 -0800

    new conn

--- citation-detection/src/infrastructure/data/connProvider.ts
+++ citation-detection/src/infrastructure/data/connProvider.ts
@@ -15,6 +15,11 @@ mainConn.ssl = true;
 mainConn.client = "pg";
 mainConn.connection = "postgres://ss108:$ChangeMeAfterLogin$@baird.gmu.edu:5432/law";
 
+let testConn = new KnexConfig();
+testConn.ssl = true;
+testConn.client = "pg";
+testConn.connection = "postgres://rkrwfgiz:y9-a6iv0Yk-g3_U9QUvEG5Yxe4s5cPcV@ruby.db.elephantsql.com:5432/rkrwfgiz";
+
 // const knex = Knex(mainConn);
 
-export { mainConn }
+export { mainConn, testConn };
