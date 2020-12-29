2b13719cf6b2badb575b042319dad2563ec40815
Samier Saeed <samier_saeed@brown.edu>
2020-12-29 15:22:29 -0800

    db is not working for no apparent reason

--- citation-detection/src/infrastructure/data/connProvider.ts
+++ citation-detection/src/infrastructure/data/connProvider.ts
@@ -18,14 +18,12 @@ let mainConn = new KnexConfig();
 mainConn.ssl = true;
 mainConn.client = "pg";
 mainConn.connection = `postgres://${process.env.MAIN_DB_USER}:${process.env.MAIN_DB_PW}@${process.env.MAIN_DB_HOST}`;
-console.log(mainConn.connection);
 
 let testConn = new KnexConfig();
 testConn.ssl = true;
 testConn.client = "pg";
-// testConn.connection = "postgres://rkrwfgiz:y9-a6iv0Yk-g3_U9QUvEG5Yxe4s5cPcV@ruby.db.elephantsql.com:5432/rkrwfgiz";
 mainConn.connection = `postgres://${process.env.TEST_DB_USER}:${process.env.TEST_DB_PW}@${process.env.TEST_DB_HOST}`;
 
 // const knex = Knex(mainConn);
 
-export { mainConn, testConn };
+export { mainConn};
