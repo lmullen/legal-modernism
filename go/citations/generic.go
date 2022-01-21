31abd955bfa122870a73f8f8bffaa9aa778f6d6a
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-01-21 15:48:11 -0500

    GenericDetector in package

---
+++ go/citations/generic.go
@@ -0,0 +1,5 @@
+package citations
+
+// GenericDetector is a generic detector with a regular expression that looks for all
+// citations
+var GenericDetector = NewDetector("Generic", `[\p{L}\s\.]{4,15}?`)
