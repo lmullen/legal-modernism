1ca48d51e400dd055cd2ca6631e53a3710ae0d43
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-01-21 16:20:30 -0500

    Script to demonstrate checking citations

---
+++ scripts/citations-regex-test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/lmullen/legal-modernism/go/citations"
+	"github.com/lmullen/legal-modernism/go/sources"
+)
+
+func main() {
+	dir, err := sources.NewDirectoryStore("data")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err := dir.GetDocByID(context.TODO(), "pretend-document.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cites := citations.GenericDetector.Detect(doc)
+	for i, cite := range cites {
+		fmt.Printf("%v: %v\n", i, cite)
+	}
+}
