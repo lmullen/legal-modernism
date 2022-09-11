bfe3a151222a26d3636ebac38bb300621340cc36
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-09-11 11:49:47 -0400

    Junk column

--- scripts/cleanup-reporters-whitelist.R
+++ scripts/cleanup-reporters-whitelist.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 
-raw <- read_csv("~/Downloads/reporters_citation_to_cap_whitelist-3.csv")
+raw <- read_csv("~/Downloads/reporters_citation_to_cap_whitelist-4.csv")
 
 raw |>
   mutate(
     statute = if_else(is.na(statute), FALSE, statute),
     uk = if_else(is.na(uk), FALSE, uk),
+    junk = if_else(is.na(junk), FALSE, junk)
   ) |>
   write_csv("~/Desktop/reporter-whitelist.csv", na = "")
