1b4b27f15cb2e6e715388daf089f9aae676c4bcc
Lincoln Mullen <lincoln@lincolnmullen.com>
2023-07-21 14:04:24 -0400

    Adjust cleanup for new whitelist

--- scripts/cleanup-reporters-whitelist.R
+++ scripts/cleanup-reporters-whitelist.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 
-raw <- read_csv("~/Downloads/reporters_citation_to_cap_whitelist-9.csv")
+raw <- read_csv("temp/whitelist.csv", na = "NULL")
 
 raw |>
   mutate(
+    reporter_found = str_trim(reporter_found),
     statute = if_else(is.na(statute), FALSE, statute),
     uk = if_else(is.na(uk), FALSE, uk),
     junk = if_else(is.na(junk), FALSE, junk)
   ) |>
-  write_csv("~/Desktop/reporter-whitelist.csv", na = "")
+  distinct(reporter_found, .keep_all = TRUE) |>
+  write_csv("temp/reporter-whitelist.csv", na = "")
