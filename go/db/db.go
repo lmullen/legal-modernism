8c9423a4acbf20e12f1d5875121c06facf609fb9
Lincoln Mullen <lincoln@lincolnmullen.com>
2025-02-14 11:29:29 -0500

    Timeout on database connection

--- go/db/db.go
+++ go/db/db.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -12,19 +13,22 @@ import (
 // Connect returns a pool of connections to the database, which is concurrency
 // safe. Uses the pgx interface.
 func Connect(ctx context.Context) (*pgxpool.Pool, error) {
+	timeout, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
 	connstr, err := getConnString()
 	if err != nil {
 		return nil, err
 	}
 
-	db, err := pgxpool.Connect(ctx, connstr)
+	db, err := pgxpool.Connect(timeout, connstr)
 	if err != nil {
-		return nil, fmt.Errorf("Error connecting to database: %w", err)
+		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 
-	err = db.Ping(ctx)
+	err = db.Ping(timeout)
 	if err != nil {
-		return nil, fmt.Errorf("Error pinging database: %w", err)
+		return nil, fmt.Errorf("error pinging database: %w", err)
 	}
 
 	return db, nil
@@ -34,7 +38,7 @@ func Connect(ctx context.Context) (*pgxpool.Pool, error) {
 func getConnString() (string, error) {
 	connstr, exists := os.LookupEnv("LAW_DBSTR")
 	if !exists {
-		return "", errors.New("Database connection string not set; use the LAW_DBSTR environment variable")
+		return "", errors.New("database connection string not set; use the LAW_DBSTR environment variable")
 	}
 	return connstr, nil
 }
