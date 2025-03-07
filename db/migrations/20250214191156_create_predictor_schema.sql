390326b31f2d1f67f8284e82355bb507b4f753d4
Lincoln Mullen <lincoln@lincolnmullen.com>
2025-03-07 11:56:44 -0500

    Remove AI predictor files from main

--- db/migrations/20250214191156_create_predictor_schema.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-SET ROLE = law_admin;
-CREATE SCHEMA IF NOT EXISTS predictor;
-
--- migrate:down
-SET ROLE = law_admin;
-DROP SCHEMA IF EXISTS predictor;
--- go/predictor/store.go
+++
@@ -1,8 +0,0 @@
-package predictor
-
-import "context"
-
-// Store is an interface describing a data store for objects relating to citations.
-type Store interface {
-	RecordBatch(ctx context.Context, b *Batch) error
-}
