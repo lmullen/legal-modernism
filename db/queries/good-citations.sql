73231e70857a82f7f3a205210954f92a008a31c5
Lincoln Mullen <lincoln@lincolnmullen.com>
2025-03-13 22:36:58 -0400

    Make names in query more consistent

--- db/queries/good-citations.sql
+++ db/queries/good-citations.sql
@@ -2,14 +2,14 @@ WITH
 	clean_citations AS (
 		SELECT
 			cites.id,
-			cites.volume AS volume_found,
-			cites.reporter_abbr AS reporter_found,
-			cites.page AS page_found,
-			clean.reporter_standard,
+			cites.volume AS cite_found_volume,
+			cites.reporter_abbr AS cite_found_reporter,
+			cites.page AS cite_found_page,
+			clean.reporter_standard AS cite_standard_reporter,
 			cites.volume || ' ' || clean.reporter_standard || ' ' || cites.page AS cite_standard,
 			cites.moml_treatise,
 			cites.moml_page,
-			cites.raw,
+			cites.raw AS cite_found,
 			clean.uk,
 			clean.statute,
 			clean.junk
@@ -22,52 +22,53 @@ WITH
 	alternative_vols AS (
 		SELECT
 			cc.id,
-			cc.volume_found,
-			cc.reporter_found,
-			cc.reporter_standard,
-			cc.page_found,
+			cc.cite_found,
+			cc.cite_found_volume,
+			cc.cite_found_reporter,
+			cc.cite_found_page,
+			cc.cite_standard_reporter,
 			cc.cite_standard,
 			cc.moml_treatise,
 			cc.moml_page,
-			cc.raw,
 			cc.uk,
 			cc.statute,
 			cc.junk,
-			vols.cap_vol || ' ' || abbr.cap_abbr || ' ' || cc.page_found AS cite_altvol,
-			abbr.cap_abbr,
-			vols.cap_vol,
-			abbr.reporter_title
+			vols.cap_vol AS cite_alternative_volume,
+			abbr.cap_abbr AS cite_alternative_reporter,
+			cc.cite_found_page AS cite_alternative_page,
+			CASE
+				WHEN vols.cap_vol IS NOT NULL
+				AND abbr.cap_abbr IS NOT NULL THEN vols.cap_vol || ' ' || abbr.cap_abbr || ' ' || cc.cite_found_page
+				ELSE NULL
+			END AS cite_alternative
 		FROM
 			clean_citations cc
-			LEFT JOIN legalhist.reporters_alt_diffvols_abbreviations abbr ON cc.reporter_standard = abbr.alt_abbr
+			LEFT JOIN legalhist.reporters_alt_diffvols_abbreviations abbr ON cc.cite_standard_reporter = abbr.alt_abbr
 			LEFT JOIN legalhist.reporters_alt_diffvols_volumes vols ON abbr.reporter_title = vols.reporter_title
-			AND cc.volume_found = vols.vol
+			AND cc.cite_found_volume = vols.vol
 	)
 SELECT
 	id,
 	moml_treatise,
 	moml_page,
-	raw AS cite_found,
-	volume_found AS cite_found_volume,
-	reporter_found AS cite_found_reporter,
-	page_found AS cite_found_page,
+	cite_found,
+	cite_found_volume,
+	cite_found_reporter,
+	cite_found_page,
 	cite_standard,
-	volume_found AS cite_standard_volume,
-	reporter_standard AS cite_standard_reporter,
-	page_found AS cite_standard_page,
-	cite_altvol AS cite_alternative,
-	cap_vol AS cite_alternative_volume,
-	cap_abbr AS cite_alternative_reporter,
-	CASE
-		WHEN cap_abbr IS NULL THEN NULL
-		ELSE page_found
-	END AS cite_alternative_page,
+	cite_found_volume AS cite_standard_volume,
+	cite_standard_reporter,
+	cite_found_page AS cite_standard_page,
+	cite_alternative,
+	cite_alternative_volume,
+	cite_alternative_reporter,
+	cite_alternative_page,
 	uk,
 	NULL AS case_id_er,
 	NULL AS case_id_cap
 FROM
 	alternative_vols
 WHERE
-	NOT junk
-	AND NOT statute
-	AND cite_standard IS NOT NULL;
+	cite_standard IS NOT NULL
+	AND NOT junk
+	AND NOT statute;
